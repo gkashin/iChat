8249cf908839284fe37eac259bed7515a00ef170
George <georgii.kashin@gmail.com>
2020-02-25 18:49:15 +0300

    Added second section of Compositional Layout

--- iChat/Controllers/ListViewController.swift
+++ iChat/Controllers/ListViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-struct MChat: Hashable {
-    var id = UUID()
+struct MChat: Hashable, Decodable {
+    var id: Int
     var username: String
-    var userImage: UIImage
+    var imageName: String
     var lastMessage: String
     
     func hash(into hasher: inout Hasher) {
@@ -25,14 +25,11 @@ struct MChat: Hashable {
 
 class ListViewController: UIViewController {
     
-    let activeChats: [MChat] = [
-        MChat(username: "George", userImage: UIImage(named: "human1")!, lastMessage: "Hello!"),
-        MChat(username: "Dmitriy", userImage: UIImage(named: "human2")!, lastMessage: "How are you?"),
-        MChat(username: "Ivan", userImage: UIImage(named: "human3")!, lastMessage: "See you!"),
-        MChat(username: "Bob", userImage: UIImage(named: "human4")!, lastMessage: "Nice to meet you!")
-    ]
+    let activeChats = Bundle.main.decode([MChat].self, from: "activeChats.json")
+    let waitingChats = Bundle.main.decode([MChat].self, from: "waitingChats.json")
     
     enum Section: Int, CaseIterable {
+        case waitingChats
         case activeChats
     }
     
@@ -71,8 +68,21 @@ class ListViewController: UIViewController {
         view.addSubview(collectionView)
         
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId2")
     }
     
+    private func reloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, MChat>()
+        snapshot.appendSections([.waitingChats, .activeChats])
+        snapshot.appendItems(waitingChats, toSection: .waitingChats)
+        snapshot.appendItems(activeChats, toSection: .activeChats)
+        
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+// MARK: - Data Source
+extension ListViewController {
     private func createDataSource() {
         dataSource = UICollectionViewDiffableDataSource<Section, MChat>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, chat) -> UICollectionViewCell? in
             guard let section = Section(rawValue: indexPath.section) else {
@@ -84,34 +94,59 @@ class ListViewController: UIViewController {
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
                 cell.backgroundColor = .systemBlue
                 return cell
+            case .waitingChats:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId2", for: indexPath)
+                cell.backgroundColor = .systemRed
+                return cell
             }
         })
     }
-    
-    private func reloadData() {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, MChat>()
-        snapshot.appendSections([.activeChats])
-        snapshot.appendItems(activeChats, toSection: .activeChats)
-        
-        dataSource.apply(snapshot, animatingDifferences: true)
-    }
-    
+}
+
+// MARK: - Setup Layout
+extension ListViewController {
     private func createCompositionalLayout() -> UICollectionViewLayout {
         let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment -> NSCollectionLayoutSection? in
             
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            guard let section = Section(rawValue: sectionIndex) else {
+                fatalError("Unknown section")
+            }
             
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(84))
-            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-            group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0)
-            
-            let section = NSCollectionLayoutSection(group: group)
-            section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 20, bottom: 0, trailing: 20)
-            return section
+            switch section {
+            case .activeChats:
+                return self.createActiveChats()
+            case .waitingChats:
+                return self.createWaitingChats()
+            }
         }
-        
         return layout
+    }
+    
+    private func createWaitingChats() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(88), heightDimension: .absolute(88))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 20
+        section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 20, bottom: 0, trailing: 20)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
+    }
+    
+    private func createActiveChats() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(78))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 8
+        section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 20, bottom: 0, trailing: 20)
+        return section
     }
 }
 
---
+++ iChat/Extensions/Bundle+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  Bundle+Extension.swift
+//  iChat
+//
+//  Created by Георгий Кашин on 25.02.2020.
+//  Copyright © 2020 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+
+        return loaded
+    }
+}
+
