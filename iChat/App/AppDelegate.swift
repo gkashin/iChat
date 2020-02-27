ac2b1096232738f74b0890da674e1a154a711cb3
George <georgii.kashin@gmail.com>
2020-02-27 18:01:25 +0300

    Added FireBase

--- iChat/App/AppDelegate.swift
+++ iChat/App/AppDelegate.swift
@@ -6,13 +6,16 @@
 //  Copyright © 2020 Georgii Kashin. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
