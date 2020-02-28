6178551179891614cbb52efb8264a18a4a706c5d
George <georgii.kashin@gmail.com>
2020-02-28 20:59:26 +0300

    Code refactoring

---
+++ iChat/Protocols/AuthNavigatingDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AuthNavigatingDelegate.swift
+//  iChat
+//
+//  Created by Георгий Кашин on 28.02.2020.
+//  Copyright © 2020 Georgii Kashin. All rights reserved.
+//
+
+protocol AuthNavigatingDelegate: class {
+    func toLoginVC()
+    func toSignUpVC()
+}
